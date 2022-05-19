c4a3638eb99d6b785a5d1fad0992f775e2d6e83d
thunchakorn <s.thunchakorn@gmail.com>
2022-05-19 15:40:04 +0700

    add MainView

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
+from re import template
 from django.urls import path
 from todo import views
 
 app_name = "todo"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.MainView.as_view(template_name='todo/main.html'), name='main'),
+    path('todolist/', views.TodoList.as_view(), name='todolist')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,42 @@
+from time import time
 from django.views import View
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from django.http import JsonResponse
+from todo.models import Todo
 
+class TodoList(LoginRequiredMixin, View):
 
-class IndexView(View):
+    model = Todo
 
     def get(self, request):
-        return render(request, 'todo/index.html')
+        due = request.GET.get("due", 'today')
+        queryset = self.model.objects.filter(owner=self.request.user).order_by('due')
+        if due is None:
+            data = queryset.values()
+            return JsonResponse({"todo_list": list(data)})
+        elif due == 'today':
+            queryset = queryset.filter(due__date=timezone.now().date())
+            data = queryset.values()
+            return JsonResponse({"todo_list": list(data)})
+        elif due == 'week':
+            today = timezone.now().date()
+            delta_day = timezone.timedelta(days=7)
+            queryset = queryset.filter(due__range=(today, today+delta_day))
+            data = queryset.values()
+            return JsonResponse({"todo_list": list(data)})
+        else:
+            data = queryset.values()
+            return JsonResponse({"todo_list": list(queryset)})
+
+
+
+class MainView(View):
+
+    template_name = ''
+
+    def get(self, request):
+        context = {'now': timezone.now()}
+        return render(request, 'todo/main.html', context)
