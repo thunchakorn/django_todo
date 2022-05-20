a1df3c4fe2dfd319628228d0c59f635d3710ffe8
thunchakorn <s.thunchakorn@gmail.com>
2022-05-20 16:55:49 +0700

    add views

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,23 @@
+from urllib import request
 from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.http import JsonResponse
-from todo.models import Todo
+from django.contrib.auth.models import User
 
+from todo.models import Todo
+from django.db.models import Q
+from todo.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+
+class MainView(View):
+
+    template_name = ''
+
+    def get(self, request):
+        ctx = {'now': timezone.now()}
+        return render(request, 'todo/main.html', ctx)
 
 class TodoList(LoginRequiredMixin, View):
 
@@ -30,11 +43,52 @@ class TodoList(LoginRequiredMixin, View):
             data = queryset.values()
             return JsonResponse({"todo_list": list(queryset)})
 
+class TodoListView(OwnerListView):
 
-class MainView(View):
-
-    template_name = ''
+    model = Todo
+    template_name = "todo/list.html"
 
     def get(self, request):
-        context = {'now': timezone.now()}
-        return render(request, 'todo/main.html', context)
+        user = User.objects.get(username=self.request.user)
+        mytodo_list = Todo.objects.filter(owner=self.request.user)
+        followtodo_list = []
+        for f_user in user.following.all():
+            followtodo_list.extend(f_user.following_user.todo_owned.all())
+
+        ctx = {"mytodo_list": mytodo_list, "followtodo_list": followtodo_list}
+        return render(request, self.template_name, ctx)
+
+class TodoDetailView(OwnerDetailView):
+
+    model = Todo
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(Q(is_public=True) | Q(owner=self.request.user))
+
+class TodoCreateView(OwnerCreateView):
+
+    model = Todo
+    fields = [
+        "name",
+        "detail",
+        "due",
+        "priority",
+        "is_public"
+    ]
+
+class TodoUpdateView(OwnerUpdateView):
+
+    model = Todo
+    fields = [
+        "name",
+        "detail",
+        "due",
+        "priority",
+        "is_public"
+    ]
+
+class TodoDeleteView(OwnerDeleteView):
+
+    model = Todo
+    template_name = "favs/delete.html"
