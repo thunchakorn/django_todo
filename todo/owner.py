98d0ca541cdaaeda52ff5c19007de025aab241b2
thunchakorn <s.thunchakorn@gmail.com>
2022-05-20 16:55:33 +0700

    add OwnerView

---
+++ todo/owner.py
@@ -0,0 +1,48 @@
+
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class OwnerListView(LoginRequiredMixin, ListView):
+    """
+    Sub-class the ListView to pass the request to the form.
+    """
+
+class OwnerDetailView(LoginRequiredMixin, DetailView):
+    """
+    Sub-class the DetailView to pass the request to the form.
+    """
+
+class OwnerCreateView(LoginRequiredMixin, CreateView):
+    """
+    Sub-class of the CreateView to automatically pass the Request to the Form
+    and add the owner to the saved object.
+    """
+
+    # Saves the form instance, sets the current object for the view, and redirects to get_success_url().
+    def form_valid(self, form):
+        object = form.save(commit=False)
+        object.owner = self.request.user
+        object.save()
+        return super().form_valid(form)
+
+
+class OwnerUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    Sub-class the UpdateView to pass the request to the form and limit the
+    queryset to the requesting user.
+    """
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Sub-class the DeleteView to restrict a User from deleting other
+    user's data.
+    """
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
